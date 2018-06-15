8783c39886dba5fe6ff5b77890a84798961264c3
JohnathanRB219 <johnathanrbaldwin@gmail.com>
2018-06-14 21:39:38 -0700

    Prompt Proj

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");{
+ alert ("Hello," + firstName + " " + lastName + " " + "you are" + " " + userAge*12+" " + "MONTHS OLD!" + " " + "and" + " " + userAge*365 + " "+ "DAYS OLD!")
+};
+document.write("Hello," + firstName + " " + lastName + " " + "you are" + " " + userAge + " "
+	+ " that equates to" + " " + userAge*12+ " " + "in months!" + " " + "and" + " " + userAge*365 + " " + "in days.")
+document.body.style.color = "cyan"
